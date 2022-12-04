7d4d3fb577eff120a7a3c1007c54d012ae727611
Casey Jacuzzi <62916413+ccjacuzzi@users.noreply.github.com>
2022-12-04 14:41:43 -0600

    Creating menus and figuring things out

--- dropship.py
+++ dropship.py
@@ -1,28 +1,87 @@
-#This is the python script for my database project
-#This is an edit
 
-#include the mysql connector library????
+#include 
 import mysql.connector
+import os
 
-#Establish the connection using the connect() constructor
+#Establish the connection to database
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="37Pickle$82"
 )
 
-#print the connection name like this <mysql.connector.connection...>
-#This is just to confirm we have a connection
+#print the connection name to confirm connection
 print(mydb)
 
-#Create a cursor object using the cursor() method of the connect class???
-mycursor = mydb.cursor()
+#Initialize Database
+def initializeDB():
+    mycursor = mydb.cursor()
+    mycursor.execute('USE dropship')
 
-#execute() method of the connect class. These execute mysql commands
-#mycursor.execute("CREATE DATABASE mydatabase")  #comment this line out after table is created
+### Function Definitions ####
+def displayMainMenu():
+    print("----Menu----")
+    print(" 1. Register a New User")
+    print(" 2. All Items ")
+    print(" 3. All Orders")
+    print(" 4. All Suppliers")
+    print(" 5. All Customers ")
+    print(" 0. End")
+    print("------------")
 
-mycursor.execute("SHOW DATABASES")
+def run():
+    print("under construction")
 
-for x in mycursor:
-    print(x)
+def exit():
+    n = int(input(" Press 0 to exit : "))
+    if n == 0:
+       os.system('cls') # For Windows
+       run()
+    else:
+       print("Invalid Option")
+       exit()
 
+def newUser():
+    mycursor = mydb.cursor()
+    print("---User Registration---\n")
+    userType = int(input("User type - 1. Customer or 2. Employee?  "))
+    if userType == 1:
+        newCustomer()
+    elif userType == 2:
+        newSupplier()
+    else:
+        print("Invalid Selection")
+        exit()
+
+def newCustomer():
+    mycursor = mydb.cursor()
+    print("-----New Customer-----")
+    name = input("Enter name: ")
+    email = input("Enter email: ")
+    #birthday = input("Enter Birthday: ")
+    sql = "INSERT INTO customer (name, email) VALUES (%s, %s)"
+    val = (name, email)
+    mycursor.execute(sql,val)
+    mydb.commit()
+    print("-----New Customer Created Successfully-----")
+    exit()
+
+def newSupplier():
+    print("Under Construction")
+
+def run():
+    displayMainMenu()
+    n = int(input("Enter option: "))
+    if n == 1:
+        os.system('cls')
+        newUser()
+    elif n == 0:
+        os.system('cls')
+        exit()
+    else:
+        os.system('cls')
+        run()
+
+if __name__ == '__main__':
+    initializeDB()
+    run()
